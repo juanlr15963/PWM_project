8b0f5afdde7d863184f5bb9ab2e7797172404e59
juanlr15963 <juanlopezra15963@gmail.com>
2026-05-03 21:02:16 +0100

    Firebase authentication working

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,23 +1,21 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
 
-// Firebase
+// Firebase imports
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
-
-// Tu environment
 import { environment } from '../environment/environment';
+import {provideHttpClient} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-
-    // FÍJATE AQUÍ: Debe ser environment.firebase (porque así lo llamaste en tu archivo)
+    // 1. PRIMERO inicializamos la App con el objeto 'firebase' de tu environment
+    // Dentro de providers en app.config.ts
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-
+    // 2. SEGUNDO proveemos el Auth
     provideAuth(() => getAuth())
   ]
 };
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,8 +2,7 @@ import { Component, inject, PLATFORM_ID, OnInit } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
-// Solo importas las funciones, no la configuración
-import { getAnalytics } from 'firebase/analytics';
+
 import { FirebaseApp } from 'firebase/app';
 
 import { HeaderComponent } from './components/header/header';
@@ -18,16 +17,6 @@ import { FooterComponent } from './components/footer/footer';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App implements OnInit {
-  private platformId = inject(PLATFORM_ID);
-  // Inyectamos la instancia que creamos en app.config.ts
-  private firebaseApp = inject<FirebaseApp>('FIREBASE_APP' as any);
-
-  ngOnInit() {
-    // Analytics solo se ejecuta en el cliente
-    if (isPlatformBrowser(this.platformId)) {
-      getAnalytics(this.firebaseApp);
-      console.log('Firebase Analytics inicializado');
-    }
-  }
+export class App {
+  // No exportamos nada
 }
